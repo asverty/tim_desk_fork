948d6afe4926c0c24c9b4d6f3fb4af4aa6137a1b
Tim <virtualxself@gmail.com>
2021-02-02 01:02:49 +0200

    Настроил открытие/закрытие меню и форм

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// DOM ELEMENTS
-const counter = document.querySelector('.counter');
-const footer = document.querySelector('.footer');
-const lastSection = Array.from(document.querySelectorAll('.section')).pop();
-
-
 // SECTION COUNTER
+
+const counter = document.querySelector('.counter');
 const numberOfSections = Array.from(document.querySelectorAll('.section')).length;
 counter.textContent = `1/${numberOfSections}`;
+
+
+
 
 
 // FULLPAGE.JS
@@ -14,7 +13,7 @@ const myFullpage = new fullpage('#fullpage', {
 	//Navigation
 	menu: '#menu',
 	lockAnchors: false,
-	anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
 	navigation: false,
 	navigationPosition: 'right',
 	navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide', 'fourthSlide'],
@@ -57,7 +56,7 @@ const myFullpage = new fullpage('#fullpage', {
 	//Design
 	controlArrows: false,
 	verticalCentered: true,
-	sectionsColor : ['#FAFAFA', '#FAFAFA', '#FAFAFA', '#FAFAFA'],
+	sectionsColor: ['#FAFAFA', '#FAFAFA', '#FAFAFA', '#FAFAFA'],
 	paddingTop: '0',
 	paddingBottom: '0',
 	fixedElements: '#header, .footer',
@@ -65,9 +64,9 @@ const myFullpage = new fullpage('#fullpage', {
 	responsiveHeight: 0,
 	responsiveSlides: false,
 	parallax: false,
-	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+	parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
 	cards: false,
-	cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+	cardsOptions: { perspective: 100, fadeContent: true, fadeBackground: true },
 
 	//Custom selectors
 	sectionSelector: '.section',
@@ -76,20 +75,96 @@ const myFullpage = new fullpage('#fullpage', {
 	lazyLoading: true,
 
 	//events
-	onLeave: function(origin, destination, direction){
+	onLeave: function (origin, destination, direction) {
 		counter.textContent = `${destination.index + 1}/${numberOfSections}`;
 	},
-	afterLoad: function(origin, destination, direction){},
-	afterRender: function(){},
-	afterResize: function(width, height){},
-	afterReBuild: function(){},
-	afterResponsive: function(isResponsive){},
-	afterSlideLoad: function(section, origin, destination, direction){},
-	onSlideLeave: function(section, origin, destination, direction){}
+	afterLoad: function (origin, destination, direction) { },
+	afterRender: function () { },
+	afterResize: function (width, height) { },
+	afterReBuild: function () { },
+	afterResponsive: function (isResponsive) { },
+	afterSlideLoad: function (section, origin, destination, direction) { },
+	onSlideLeave: function (section, origin, destination, direction) { }
 });
 
 
+
+
+
+// HEADER
+
+const headerLogo = document.querySelector('#header-logo');
+const buttons = document.querySelectorAll('.header__button');
+const headerBrandsButton = document.querySelector('#header-brands-button');
+const headerCategoriesButton = document.querySelector('#header-categories-button');
+const headerSubscribeButton = document.querySelector('#header-subscribe-button');
+const headerLogInButton = document.querySelector('#header-login-button');
+const headerSignInButton = document.querySelector('#header-signin-button');
+const headerForgotPasswordButton = document.querySelector('#header-forgot-password-button');
+const headerWishesButton = document.querySelector('#header-wishes-button');
+const headerCartButton = document.querySelector('#header-cart-button');
+const menus = document.querySelectorAll('.menu');
+const brandsMenu = document.querySelector('#brands-menu');
+const categoriesMenu = document.querySelector('#categories-menu');
+const forms = document.querySelectorAll('.form');
+const signInForm = document.querySelector('#signin-form');
+const logInForm = document.querySelector('#login-form');
+const subscribeForm = document.querySelector('#subscribe-form');
+const passwordRecoveryForm = document.querySelector('#password-recovery-form');
+const sections = document.querySelectorAll('.section');
+
+function headerTurnsBlack() {
+	headerLogo.classList.remove('header__logo_white');
+	buttons.forEach(button => button.classList.remove('header__button_white'));
+};
+
+function headerTurnsWhite() {
+	headerLogo.classList.add('header__logo_white');
+	buttons.forEach(button => button.classList.add('header__button_white'));
+};
+
+function showPopup(popup, hidingClass) {
+	headerTurnsBlack();
+	popup.classList.remove(hidingClass);
+}
+
+function hideAllPopups() {
+	menus.forEach(menu => menu.classList.add('menu_hidden'));
+	forms.forEach(form => form.classList.add('form_hidden'));
+	headerTurnsWhite();
+};
+
+function togglePopup(popup, hidingClass) {
+	if (popup.classList.contains(hidingClass)) {
+		hideAllPopups();
+		showPopup(popup, hidingClass);
+	} else hideAllPopups();
+};
+
+headerBrandsButton.addEventListener('click', () => togglePopup(brandsMenu, 'menu_hidden'));
+headerCategoriesButton.addEventListener('click', () => togglePopup(categoriesMenu, 'menu_hidden'));
+headerSubscribeButton.addEventListener('click', () => togglePopup(subscribeForm, 'form_hidden'));
+headerLogInButton.addEventListener('click', () => togglePopup(logInForm, 'form_hidden'));
+headerSignInButton.addEventListener('click', () => togglePopup(signInForm, 'form_hidden'));
+headerForgotPasswordButton.addEventListener('click', () => togglePopup(passwordRecoveryForm, 'form_hidden'));
+
+sections.forEach(section => {
+	section.addEventListener('click', () => hideAllPopups());
+});
+
+document.addEventListener('keydown', event => {
+	if (event.key == 'Escape') hideAllPopups();
+});
+
+
+
+
+
 // FOOTER
+
+const footer = document.querySelector('.footer');
+const lastSection = Array.from(document.querySelectorAll('.section')).pop();
+
 function showFooter() {
 	footer.classList.remove('footer_hidden');
 	myFullpage.setAllowScrolling(false, 'up');
@@ -113,7 +188,7 @@ document.addEventListener('keydown', event => {
 // close footer via keyboard
 document.addEventListener('keydown', event => {
 	let activeSection = myFullpage.getActiveSection();
-	if ((activeSection.isLast) && (event.key == 'ArrowUp')) hideFooter();
+	if ((activeSection.isLast) && ((event.key == 'ArrowUp') || event.key == 'Escape')) hideFooter();
 });
 
 // open footer via mouse wheel
@@ -134,4 +209,19 @@ footer.addEventListener('wheel', event => {
 // close footer via click on section
 lastSection.addEventListener('click', () => {
 	if (!footer.classList.contains('footer_hidden')) hideFooter();
+});
+
+// close all menus and forms via click on footer
+footer.addEventListener('click', () => {
+	hideAllPopups();
+});
+
+// close footer via click on menu
+menus.forEach(menu => {
+	menu.addEventListener('click', () => hideFooter());
+});
+
+// close footer via click on form
+forms.forEach(form => {
+	form.addEventListener('click', () => hideFooter());
 });
